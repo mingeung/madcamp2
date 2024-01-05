a5debb34dc48d46d66917871afa8f88279f42b4d
mingeung <jmk2229@naver.com>
2024-01-05 17:21:05 +0900

    navigation_bar_structure

--- app/src/main/java/com/example/madcampweek2/MainActivity.kt
+++ app/src/main/java/com/example/madcampweek2/MainActivity.kt
@@ -3,28 +3,56 @@ package com.example.madcampweek2
 import SectionsPagerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madcampweek2.databinding.ActivityMainBinding
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewPager: ViewPager2
-    private lateinit var tabLayout: TabLayout
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        viewPager = findViewById(R.id.viewPager)
-        tabLayout = findViewById(R.id.tabLayout)
+        setBottomNavigationView()
 
-        // Set up the ViewPager with the sections adapter.
-        viewPager.adapter = SectionsPagerAdapter(this)
+        // 앱 초기 실행 시 홈화면으로 설정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment1_home
+        }
+    }
 
-        // Connect the TabLayout with the ViewPager
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = "Tab ${(position + 1)}"
-        }.attach()
+    fun setBottomNavigationView() {
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            try {
+                when (item.itemId) {
+                    R.id.fragment1_home -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_Container, Fragment1_Home()).commit()
+                        return@setOnNavigationItemSelectedListener true
+                    }
+
+                    R.id.fragment2_community -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_Container, Fragment2_Community()).commit()
+                        return@setOnNavigationItemSelectedListener true
+                    }
+
+                    R.id.fragment3_profile -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_Container, Fragment3_Profile()).commit()
+                        return@setOnNavigationItemSelectedListener true
+                    }
+
+                    else -> return@setOnNavigationItemSelectedListener false
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return@setOnNavigationItemSelectedListener false
+            }
+        }
     }
 }
