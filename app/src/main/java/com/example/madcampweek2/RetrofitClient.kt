5c9d6a72e40e050ad98d43ae45109494ab5f876f
kesler1202 <kesler@kaist.ac.kr>
2024-01-10 10:37:27 +0900

    duplicated token deleted

--- app/src/main/java/com/example/madcampweek2/RetrofitClient.kt
+++ app/src/main/java/com/example/madcampweek2/RetrofitClient.kt
@@ -14,29 +14,6 @@ object RetrofitClient {
 
     fun getInstance(context: Context): ApiService {
         if (apiService == null) {
-            val logging = HttpLoggingInterceptor().apply {
-                setLevel(HttpLoggingInterceptor.Level.BODY)
-            }
-
-            // Token Interceptor
-            val tokenInterceptor = okhttp3.Interceptor { chain ->
-                val originalRequest = chain.request()
-                val sharedPrefs = context.getSharedPreferences("YourSharedPrefs", Context.MODE_PRIVATE)
-                val token = sharedPrefs.getString("authToken", null)
-
-                val newRequest = if (token != null) {
-                    val requestWithToken = originalRequest.newBuilder()
-                        .addHeader("Authorization", "Token $token")
-                        .build()
-                    Log.d("OkHttp", "Sending request with token: $token")
-                    requestWithToken
-                } else {
-                    originalRequest
-                }
-
-                chain.proceed(newRequest)
-            }
-
             val client = OkHttpClient.Builder()
                 .addInterceptor { chain ->
                     val originalRequest = chain.request()
